3dece830c214cdd8ca40482b0297e815187340c2
Colin McDonnell <colinmcd@alum.mit.edu>
2020-11-03 14:54:38 -0500

    Added id function tests

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -73,3 +73,42 @@ test('custom id multikey', () => {
   expect(val.value.instrument).toEqual('flugelhorn');
   expect(appleColin.value.username).toEqual('colin');
 });
+
+test('custom id function', () => {
+  const normi = new Normi({
+    id: data => {
+      return `${data.username || Math.random()}__${data.domain ||
+        Math.random()}`;
+    },
+  });
+
+  normi.merge({
+    username: 'colin',
+    domain: 'google.com',
+    first: 'tuna',
+  });
+
+  const appleColin = normi.merge({
+    username: 'colin',
+    domain: 'apple.com',
+    second: 'frankenstein',
+  });
+
+  // no domain
+  normi.merge({
+    username: 'colin',
+    stone: 'blarney',
+  });
+
+  const val: any = normi.merge({
+    username: 'colin',
+    domain: 'google.com',
+    instrument: 'flugelhorn',
+  });
+
+  expect(val.value.username).toEqual('colin');
+  expect(val.value.domain).toEqual('google.com');
+  expect(val.value.first).toEqual('tuna');
+  expect(val.value.instrument).toEqual('flugelhorn');
+  expect(appleColin.value.username).toEqual('colin');
+});
